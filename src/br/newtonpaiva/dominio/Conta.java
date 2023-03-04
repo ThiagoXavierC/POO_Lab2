aba96e2fd9b153825f848e97168f67dcb0e46aa5
12119809 <12119809@aluno.newtonpaiva.br>
2023-03-03 21:21:40 -0300

    feat: implementa construtores equals e hashcode

--- src/br/newtonpaiva/dominio/Conta.java
+++ src/br/newtonpaiva/dominio/Conta.java
@@ -1,25 +1,43 @@
 package br.newtonpaiva.dominio;
 
+import java.util.Objects;
+
 public class Conta {
     private Integer numero;
     private Double saldo;
 
-    public Double sacar(Double valor){
+    public Conta() {
+        this(null);
+    }
+
+    public Conta(Integer numero) {
+        this(numero, 0.0);
+
+    }
+
+    public Conta(Integer numero, Double saldo) {
+        this.numero = numero;
+        this.saldo = saldo;
+    }
+
+    public Double sacar(Double valor) {
+
         return 0.0;
     }
 
-    public Double depositar(Double valor){
-        if(valor <= 0)
+    public Double depositar(Double valor) {
+        if (valor <= 0)
             throw new IllegalArgumentException("Valor menor ou igual a 0");
         saldo += valor;
         return saldo;
     }
 
-    public Integer getNumero(){
+    public Integer getNumero() {
+
         return numero;
     }
 
-    public void setNumero(Integer numero){
+    public void setNumero(Integer numero) {
         this.numero = numero;
     }
 
@@ -29,5 +47,21 @@ public class Conta {
 
     public void setSaldo(Double saldo) {
         this.saldo = saldo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+
+
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Conta conta = (Conta) o;
+        return Objects.equals(numero, conta.numero);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numero);
     }
 }
